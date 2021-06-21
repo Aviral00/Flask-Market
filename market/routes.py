f8de27e517aea4645bfb703efc91c5899d0bd16b
Aviral00 <aviralsrivastava1404@gmail.com>
2021-06-22 02:10:24 +0530

    First Commit

---
+++ market/routes.py
@@ -0,0 +1,34 @@
+from market import app
+from flask import render_template, redirect, url_for
+from market.models import Item, User
+from market.forms import RegisterForm
+from market.models import db
+
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+
+@app.route('/market')
+def market_page():
+    items = Item.query.all()
+    return render_template('market.html', items=items)
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_on_submit = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data
+                            )
+        db.session.add(user_on_submit)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            print(f'There was an error : {err_msg}')
+    return render_template('register.html', form=form)
